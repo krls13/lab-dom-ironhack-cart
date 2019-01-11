3c2ae8b6fffa3a4cff44e03981504213946761bc
krls13 <krlsalberto13@hotmail.com>
2019-01-11 21:05:48 +0100

    Iteration 2 100%

---
+++ starter-code/js/index.js
@@ -0,0 +1,69 @@
+function deleteItem(e){
+  console.log("delete");
+}
+
+function getPriceByProduct(itemNode){
+
+}
+
+function updatePriceByProduct(productPrice, index){
+
+}
+
+
+
+function getTotalPrice() { 
+  sum = 0
+  for(i = 0; i <= 1; i++){
+    var precioElemento = parseInt(document.getElementsByClassName('price')[i].innerHTML.slice(1,3))
+    var cantidadElemento = parseInt(document.getElementsByClassName("quantity")[i].value)
+
+    if(cantidadElemento > 0){
+      sum = precioElemento * cantidadElemento
+    }
+    else {
+      sum = 0
+    }
+    console.log(sum)
+    document.getElementsByClassName("totalPrice")[i].innerHTML = "$" + sum + ".00"
+  }
+  
+}
+
+function createQuantityInput(){
+
+}
+
+function createDeleteButton(){
+
+}
+
+function createQuantityNode(){
+
+}
+
+function createItemNode(dataType, itemData){
+
+}
+
+function createNewItemRow(itemName, itemUnitPrice){
+
+}
+
+function createNewItem(){
+
+}
+
+window.onload = function(){
+  var calculatePriceButton = document.getElementById('calc-prices-button');
+  //var createItemButton = document.getElementById('new-item-create');
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+
+  calculatePriceButton.onclick = getTotalPrice;
+  //createItemButton.onclick = createNewItem;
+
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
+
+};
