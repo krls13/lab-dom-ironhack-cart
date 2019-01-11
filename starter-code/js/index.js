33c0a555621740c3660fcdf8969fda6bc1c8641a
krls13 <krlsalberto13@hotmail.com>
2019-01-11 22:11:11 +0100

    iteration 3 100%

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,29 +3,32 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
+    var elementTotalPrice = 0
 
+    var precioElemento = parseInt(document.getElementsByClassName('price')[itemNode].innerHTML.slice(1,3))
+    var cantidadElemento = parseInt(document.getElementsByClassName("quantity")[itemNode].value)
+
+    if(cantidadElemento > 0){
+      elementTotalPrice = precioElemento * cantidadElemento
+    }
+    
+    document.getElementsByClassName("totalPrice")[itemNode].innerHTML = "$" + elementTotalPrice + ".00"
+
+    return elementTotalPrice
 }
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
-
-
 function getTotalPrice() { 
-  sum = 0
-  for(i = 0; i <= 1; i++){
-    var precioElemento = parseInt(document.getElementsByClassName('price')[i].innerHTML.slice(1,3))
-    var cantidadElemento = parseInt(document.getElementsByClassName("quantity")[i].value)
+  var totalPrice = 0
 
-    if(cantidadElemento > 0){
-      sum = precioElemento * cantidadElemento
-    }
-    else {
-      sum = 0
-    }
-    document.getElementsByClassName("totalPrice")[i].innerHTML = "$" + sum + ".00"
-  } 
+  for(i = 0; i <= 1; i++){
+    totalPrice += getPriceByProduct(i)
+  }
+
+  document.getElementById('final-price').innerHTML = "$" + totalPrice
 }
 
 function createQuantityInput(){
